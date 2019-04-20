2c0490e79a9ceb86a9e6f42a13e19a128bd46fe6
Daniel Neves <danielncamargo@gmail.com>
2019-04-20 20:57:34 -0300

    Update compile.bat

--- Trab01ALUSimulation/compile.bat
+++ Trab01ALUSimulation/compile.bat
@@ -1,6 +1,5 @@
 gcc -c alusimulation.c
 gcc -c processorarchitecture.c
 gcc -c baseconv.c
-gcc -c system.c
 gcc -c main.c
-gcc -o main main.c baseconv.c processorarchitecture.c alusimulation.c system.c -lm
+gcc -o main main.c baseconv.c processorarchitecture.c alusimulation.c -lm
