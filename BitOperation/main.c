6477d2ab03ac11cbfa05e37b04e2b4025f9217ca
Daniel Neves <danielncamargo@gmail.com>
2019-04-26 19:37:52 -0300

    update main.c

--- BitOperation/main.c
+++ BitOperation/main.c
@@ -1,8 +1,63 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #define BINMAX 32
+#define OFF '0'
+#define ON '1'
+
+int scanftob(int*);
+
+int isbin(char*);
+
 int main(){
-    typedef unsigned int byte;
-    byte[BINMAX] reg0;
-    byte[BINMAX] reg1;
-    
+    typedef unsigned int bit;
+    bit reg[BINMAX];
+    scanftob(reg);
+/*
+    reg[0] = 0;
+    reg[0] = !reg0[0];
+    printf("%d\n", reg[0]);*/
 }
+
+int scanftob(int* vbit) {
+    char bitarray[BINMAX];
+    memset(bitarray, OFF, BINMAX);
+    fgets(bitarray, BINMAX, stdin);
+    printf("%d\n", strlen(bitarray));
+    int i;
+    for(i = 0; i < strlen(bitarray); i++) {
+        printf("%c", bitarray[i]);
+    }
+
+
+    if(isbin(bitarray)){
+        printf("is binary\n");
+    } else {
+        printf("is not binary\n");
+    }
+}
+
+int isbin(char* bitarray) {
+    int i;
+    for (i = 0; i < BINMAX; i++) {
+        if((bitarray[i] != ON) && (bitarray[i] != OFF)){
+            return 0;
+        }
+    }
+    return 1;
+}
+
+/*
+int isbin(int bin) {
+    int i, j;
+    for (i = 0; i < bin; i++) {
+        j = bin % 10;
+        if((j != 0) || (j != 1)){
+            return 0;
+        }
+        bin = bin/10;
+    }
+    return 1;
+}
+*/
